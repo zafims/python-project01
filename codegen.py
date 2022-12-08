97aea55535f83019cce24c4c70554cb803e22153
zafims <007zafar@gmail.com>
2022-12-08 14:17:02 +0600

    first commit

---
+++ codegen.py
@@ -0,0 +1,19 @@
+import random
+
+print('Welcome To Your Password Generator ')
+
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@£$%^&*().,?0123456789'
+
+number = input('Amount of passwords to generate: ')
+number = int(number)
+
+length = input('Input your password length: ') 
+length = int(length)
+
+print('\nHere are your passwords :')
+
+for pwd in range(number):
+ passwords = ''
+ for c in range(length):
+     passwords += random.choice (chars)
+print(passwords)
